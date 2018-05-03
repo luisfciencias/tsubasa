a7bcb501a819e92c5f3fb25ee35442768502c1ce
Luis Torres <luisfciencias@gmail.com>
2018-05-03 17:00:24 +1000

    NEW: Include scipy spherical bessel function

--- test.py
+++ test.py
@@ -1,7 +1,21 @@
 import numpy as np
 import scipy
 import matplotlib.pyplot as plt
+from scipy.special import spherical_jn
 
 # definition of the spherical bessel function
 def bessel_j0(x):
     return np.sin(x)/x
+
+# definition of 
+x = np.linspace(0,10,100)
+y = np.sin(x)/x
+y1 = spherical_jn(0, x)
+
+plt.plot(x, y, 'r--', label='Analytical')
+plt.plot(x, y1, 'b:', label='Numerical')
+plt.xlim(0, 10)
+plt.xlabel('$x$')
+plt.ylabel('$j_0$')
+plt.legend()
+plt.show()
