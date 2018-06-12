446a7c444090c55c1feff386982264d866df304b
Luis Torres <luisfciencias@gmail.com>
2018-06-12 15:34:00 +1000

    NEW: Add spherical Bessel function y_n via a tools file

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.special import spherical_jn
+from tools import bessel_yn
 
 
 # definition of the spherical bessel function
@@ -12,11 +13,14 @@ def bessel_j0(p):
 x = np.linspace(0, 10, 100)
 y = np.sin(x)/x
 y1 = spherical_jn(0, x)
+y2 = bessel_yn(0, x)
 
-plt.plot(x, y, 'r--', label='Analytical')
-plt.plot(x, y1, 'b:', label='Numerical')
+plt.plot(x, y, 'r--', label='Analytical $j_0$')
+plt.plot(x, y1, 'b:', label='Numerical $j_0$')
+plt.plot(x, y2, 'g--', label='y0')
+
 plt.xlim(0, 10)
 plt.xlabel('$x$')
-plt.ylabel('$j_0(x)$')
+plt.ylabel('$j_0(x), y_0(x)$')
 plt.legend()
 plt.savefig('plot_bessel.png', dpi=128)
