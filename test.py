720049a491b6c05f7a5703275795f6a8754af915
Luis Torres <luisfciencias@gmail.com>
2018-06-12 15:22:50 +1000

    NEW: Save plot to output file

--- test.py
+++ test.py
@@ -1,14 +1,15 @@
 import numpy as np
-import scipy
 import matplotlib.pyplot as plt
 from scipy.special import spherical_jn
 
+
 # definition of the spherical bessel function
-def bessel_j0(x):
-    return np.sin(x)/x
+def bessel_j0(p):
+    return np.sin(p)/p
+
 
 # definition of 
-x = np.linspace(0,10,100)
+x = np.linspace(0, 10, 100)
 y = np.sin(x)/x
 y1 = spherical_jn(0, x)
 
@@ -16,6 +17,6 @@ plt.plot(x, y, 'r--', label='Analytical')
 plt.plot(x, y1, 'b:', label='Numerical')
 plt.xlim(0, 10)
 plt.xlabel('$x$')
-plt.ylabel('$j_0$')
+plt.ylabel('$j_0(x)$')
 plt.legend()
-plt.show()
+plt.savefig('plot_bessel.png', dpi=128)
